213f2a5126a91fdf2517f9a4cd8c7221f7a9ff63
fdumont <fj.dumont@gmail.com>
2022-11-13 17:45:17 +0100

    Refactor

--- cmd/clifford/main.go
+++ cmd/clifford/main.go
@@ -8,12 +8,14 @@ import (
 	"time"
 )
 
+const (
+	DotsPerCm = 119 // 300 DPI
+	Size      = 20 * DotsPerCm
+	Steps     = 1e8
+)
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-
-	size := 1024
-	stabSteps := 1e4
-	steps := int(1e8)
 
 	log.Println("fetching random gradient...")
 	grad, err := randomGradient()
@@ -25,19 +27,19 @@ func main() {
 	att := clifford.NewAttractor(a, b, c, d)
 
 	log.Println("stabilizing attractor...")
-	for i := 0; i < int(stabSteps); i++ {
+	for i := 0; i < 128; i++ {
 		att.Advance()
 	}
 
 	log.Println("building histogram...")
-	hist := clifford.NewHistogram(size, math.Phi/2, att)
-	for i := 0; i < steps; i++ {
+	hist := clifford.NewHistogram(Size, math.Phi/2, att)
+	for i := 0; i < Steps; i++ {
 		att.Advance()
 		hist.Inc(att.X, att.Y)
 	}
 
 	log.Println("rendering histogram...")
-	img := clifford.RenderHistogram(hist, size, grad)
+	img := clifford.RenderHistogram(hist, Size, grad)
 
 	log.Println("writing output image...")
 	if err := writeImage("./output.jpg", img); err != nil {
